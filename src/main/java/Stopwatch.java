12412d3f80126e04689ec5c0a55cf4cfad49a5ed
rasool <rasools27@gmail.com>
2018-08-16 18:51:49 +0530

    add files

---
+++ src/main/java/Stopwatch.java
@@ -0,0 +1,19 @@
+
+public class Stopwatch {
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double time() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/result.java
@@ -0,0 +1,29 @@
+
+public class result {
+
+	public static String size1() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String size() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String size111() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String size11() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String size1111() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
